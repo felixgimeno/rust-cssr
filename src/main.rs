159fef174c818157108f07396f3fa068380dc5f4
keur <z95341@gmail.com>
2017-03-19 20:14:33 +0100

    add my own chi_square

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-extern crate nalgebra;
-use nalgebra::{DVector};
-	
+
+
 fn main() {
     println!("Hello, world!");
-    
+
 }
 
 fn get_n_gram_id(previous: u32, new_letter: u32, alphabet_size: u32) -> u32 {
-	alphabet_size*previous + new_letter
-	}
+    alphabet_size * previous + new_letter
+}
 
-fn chi_square(a: &DVector<f32>, b: &DVector<f32>, alpha_thresh: f32) -> bool {
-	let small : f32 = 0.0001;
-	let c : DVector<f32> = a-b;
-	let d : DVector<f32> = c.component_mul(&c);
-	let e : DVector<f32> = d.component_div(&b.map(|x| if x > small {x} else {small}));
-	let f : f32 = (e.iter()).sum();
-	f <= alpha_thresh
+fn chi_square(a: Vec<f32>, b: Vec<f32>, alpha_thresh: f32) -> bool {
+    let small: f32 = 0.0001;
+    let mut sum: f32 = 0f32;
+    match a.len() == b.len() {
+        true => {
+            for i in 0usize..a.len() {
+                let diff = a[i] - b[i];
+                let sqr = diff * diff;
+                let divi = sqr / if b[i] > small { b[i] } else { small };
+                sum += divi;
+            }
+            sum <= alpha_thresh
+        }
+        _ => panic!(),
+    }
 }
